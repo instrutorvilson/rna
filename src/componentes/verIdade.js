8b6849457a0aa92ab8b1f596b3c7a65a6c68c8f7
Vilson Moro <vilson.moro@PROWAY.TREINA>
2023-11-18 15:50:18 -0300

    Adicionar funcionalidade ao componente verIdade

--- src/componentes/verIdade.js
+++ src/componentes/verIdade.js
@@ -1,27 +1,45 @@
+import { useState } from 'react';
 import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import styles from '../estilos/estilos';
 
 export default function VerIdade() {
+  const [nome, setNome] = useState('')
+  const [idade, setIdade] = useState('')
+  const [resultado, setResultado] = useState()
+
+  function handleVerificar(){
+    if(idade < 18){
+        setResultado('menor')
+    }
+    else{
+        setResultado('maior')
+    }
+  }
+
   return (
     <View>      
       <TextInput 
          style={styles.input}
-         placeholder='Informe seu nome'          
+         placeholder='Informe seu nome' 
+         value={nome} 
+         onChangeText={(text)=>setNome(text)}       
       />
 
       <TextInput 
          style={styles.input}
-         placeholder='Informe seu idade'          
+         placeholder='Informe seu idade'
+         value={idade}
+         onChangeText={(text)=>setIdade(text)}          
       />
 
       <TouchableOpacity
           style={styles.btacao}
-          onPress={()=>alert('')}
+          onPress={handleVerificar}
       >
           <Text>Verificar</Text>
       </TouchableOpacity>  
       <View>
-           <Text style={styles.resultado}>Vc é: </Text>
+           <Text style={styles.resultado}>{nome} vc é {resultado} de idade.</Text>
       </View>       
       
     </View>
