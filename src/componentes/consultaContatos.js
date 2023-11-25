b852d27f3e9736efc857b00757713afe14fbe2f4
Vilson Moro <vilson.moro@PROWAY.TREINA>
2023-11-25 17:34:32 -0300

    Aula 2 - consultar dados do firebase

--- src/componentes/consultaContatos.js
+++ src/componentes/consultaContatos.js
@@ -1,14 +1,28 @@
 
+import { useEffect, useState } from 'react';
 import { FlatList, Text, View } from 'react-native';
 import styles from '../estilos/estilos';
 import CardContato from './cardContato';
+import app from '../configuracao/firebaseConfig'
+import { doc, getFirestore, collection, getDocs, query } from "firebase/firestore";
 
 export default function ConsultaContatos() {
-  const contatos = [
+ /* const contatos = [
     {id:1, nome:'Maria', email:'maria@gmail.com', fone:'(47)9087-0987'},
     {id:2, nome:'João', email:'joao@gmail.com', fone:'(47)5698-0987'},
     {id:3, nome:'Pedro', email:'pedro@gmail.com', fone:'(47)5698-0987'}
-]
+   ]*/
+  const[contatos, setContatos] = useState([])
+
+  const db = getFirestore(app)
+  
+  async function loadFirebase(){
+      let query = await getDocs(collection(db,"contatos"))
+      query.forEach((doc) => setContatos((ant)=>[...ant,doc.data()]))
+  }
+
+  useEffect(()=>{loadFirebase()},[])
+
   return (
     <View style={styles.container}>
       <FlatList 
