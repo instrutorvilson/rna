0733f3742074dace5c1be7fee9c2367c9cdbce94
Vilson Moro <vilson.moro@PROWAY.TREINA>
2024-01-06 16:09:23 -0300

    Adicionar icone na opção de menu

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome'
 import 'react-native-gesture-handler';
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { NavigationContainer } from '@react-navigation/native';
@@ -15,9 +17,21 @@ export default function App() {
     <>
      <NavigationContainer>
           <Drawer.Navigator>
-              <Drawer.Screen name="Home" component={Home}/>
-              <Drawer.Screen name="Cadastro" component={Cadastro}/>
-              <Drawer.Screen name="Consulta" component={Consulta}/>
+              <Drawer.Screen 
+                 name="Home" 
+                 component={Home}
+                 options={{ drawerIcon:({color, size}) => <Icon name='home' size={size} color={color}/> }}
+              />
+              <Drawer.Screen 
+                  name="Cadastro" 
+                  component={Cadastro}
+                  options={{ drawerIcon:({color, size}) => <Icon name='plus' size={size} color={color}/> }}
+                />
+              <Drawer.Screen 
+                  name="Consulta" 
+                  component={Consulta}
+                  options={{ drawerIcon:({color, size}) => <Icon name='search' size={size} color={color}/> }}
+              />
           </Drawer.Navigator>
        </NavigationContainer>      
       <StatusBar style="auto" />
